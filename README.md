ce28eeaa220abd2f3897624572753663fe747dcb
范文华 <531362022@qq.com>
2019-09-08 10:37:15 +0800

    feat: First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Installation
+
+```bash
+yarn add @rnui/text
+```
+
+## Usage
+```typescript jsx
+import React from 'react';
+import { UIText } from '@rnui/text';
+
+const App = () => {
+  return <UIText>Hello World</UIText>;
+};
+```
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react';
+import { Text, TextProps, TextStyle } from 'react-native';
+
+interface Props extends TextProps {
+    color?: string;
+    size?: number;
+    weight?: TextStyle['fontWeight'];
+    lineHeight?: number;
+}
+
+export const UIText: FunctionComponent<Props> = (props) => {
+    const { color, size, weight, lineHeight, style, ...rest } = props;
+
+    return <Text style={[{ color, fontSize: size, lineHeight, fontWeight: weight }, style]} {...rest} />;
+};
+
+UIText.defaultProps = {
+    color: '#333',
+    size: 14,
+    weight: 'normal',
+    lineHeight: 20,
+};
